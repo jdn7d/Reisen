61dc7079eeb9d6a5ebf9448afeebb714aa33bd4a
jessie <jssdnvn@gmail.com>
2021-04-20 13:22:41 -0500

    removed title from destinatino.js

--- frontend/src/Components/Destination.js
+++ frontend/src/Components/Destination.js
@@ -3,7 +3,7 @@ export default function Destination({destination, callDispatch}) {
 
     return (
         <div className="destination">
-            "Destination View"
+           
            <p onClick={() => callDispatch(destination)}>{destination.city}, {destination.country} <br></br>Dates: {destination.date} <br></br>Activities: {destination.activities} </p>
         </div>
     )
