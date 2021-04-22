8b63a3b37fde00310abc1543fe32ac52c1c85939
jessie <jssdnvn@gmail.com>
2021-04-22 16:47:11 -0500

    increment form and button working

--- frontend/src/Components/Destination.js
+++ frontend/src/Components/Destination.js
@@ -3,30 +3,23 @@ import React from 'react'
 
 export default class Destination extends React.Component {
 
+  
+
     state = {
-        likes: 0,
-        increment: 1
+        likes: 0
     }
 
     handleClick = () => {
-      console.log(this.state.likes)
-        console.log(this.state.increment)
-        this.setState({likes: this.state.likes + this.state.increment})
+      const incrementNum = parseInt(this.props.increment, 10)
+      this.setState({likes: this.state.likes + incrementNum})
     }
 
-    handleChange = event => {
-      this.setState({[event.target.title]: event.target.value})
-    }
+  
     
     render() {
         return (
           <div className="destination">
-            <input type="number"
-            title="increment"
-            onChange={this.handleChange}
-            value={this.state.increment}
-            placeholder='1'
-            /> 
+        
             <p onClick={() => this.props.callDispatch(this.props.destination)}>{this.props.destination.city}, {this.props.destination.country} <br></br>Dates: {this.props.destination.date} <br></br>Activities: {this.props.destination.activities} </p>
            
 
--- frontend/src/Components/DestinationsList.js
+++ frontend/src/Components/DestinationsList.js
@@ -5,15 +5,30 @@ import Destination from './Destination'
 
 class DestinationsList extends React.Component {
 
+    state = {
+        increment: 1
+    }
+
+    handleChange = event => {
+        this.setState({[event.target.title]: event.target.value})
+      }
+
+
     renderDestinations = () => {
         return this.props.destinations.map(destination => {
-            return <Destination destination={destination} callDispatch={this.callDispatch} />
+            return <Destination increment={this.state.increment} destination={destination} callDispatch={this.callDispatch} />
         })
     }
 
     render() {
         return (
             <div className="list-container">
+                <input type="number"
+                    title="increment"
+                    onChange={this.handleChange}
+                    value={this.state.increment}
+                    placeholder='1'
+                /> 
                    {this.renderDestinations()}
             </div>
         )
